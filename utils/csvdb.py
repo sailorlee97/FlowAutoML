58bf56e85c9a4d3a6da43af6aeafdb6c9426d476
Zeyi <50068428+sailorlee97@users.noreply.github.com>
2022-07-06 22:45:10 +0800

    Update csvdb.py

--- utils/csvdb.py
+++ utils/csvdb.py
@@ -40,39 +40,16 @@ class ConnectMysql:
         self.server.close()
         print('close success!')
 
-    def run_server_connect_mysql(self):
-
-        # server = SSHTunnelForwarder(
-        #     ssh_address_or_host=(self.opt.ip, self.opt.port),
-        #     ssh_username=self.opt.server_username,
-        #     ssh_password=self.opt.server_password,
-        #     remote_bind_address=('localhost', 3306)
-        # )
-        # self.server.start()
-        # db = pymysql.connect(
-        #     host='127.0.0.1',
-        #     port=server.local_bind_port,
-        #     user=self.opt.database_username,
-        #     passwd=self.opt.database_username_password,
-        #     db=self.opt.databaseName,
-        #     charset='utf8'
-        # )
-        #        cursor = self.connect.cursor()
-        # 使用execute()方法执行SQL查询
-        self.cursor.execute("SELECT VERSION()")
-
-        # todo 存 （羊） 写（刘）
-
-        data = self.cursor.fetchone()
-        print("Database version : %s " % data)
-        self.run()
-
     def read_csv_columns(self):
 
-        # todo 这里对接王梓炫组，下面代码仅供算法组测试
-        # 读取csv文件的列索引，用于建立数据表时的字段
+        # todo 这里对接王梓炫组，读取数据的代码仅供算法组测试，
+        #  这里面需要传递的一个dataframe格式的数据
+
         csv_name = 'data/CICIDS_test.csv'
         data = pd.read_csv(csv_name, encoding="utf-8")
+
+        # 这里不变
+
         columns = data.columns.tolist()
         types = data.dtypes
         make_table = []
@@ -106,9 +83,13 @@ class ConnectMysql:
 
     def read_csv_values(self):
 
-        # todo 这里对接王梓炫组，下面代码仅供算法组测试
+        # todo 这里对接王梓炫组，读取数据的代码仅供算法组测试，
+        #  这里面需要传递的一个dataframe格式的数据
+
         csv_name = 'data/CICIDS_test.csv'
         data = pd.read_csv(csv_name)
+
+        #这里不需要变化
         print('the shape of data we get.',data.shape)
         data.replace([np.inf, -np.inf], np.nan, inplace=True)
         data.dropna(inplace=True)
@@ -120,13 +101,11 @@ class ConnectMysql:
 
     def write_mysql(self):
 
-        # table_name = 't_cicids'
-        # columns = self.read_csv_colnmus()
         data = self.read_csv_values()
         for i in data:
             data_6 = tuple(i)
             sql = 'insert into {} values{}'.format(self.opt.tableName, data_6)
-            print(sql)
+            #print(sql)
             self.cursor.execute(sql)
             self.commit()
         print("\nComplete write data operation!")
@@ -136,10 +115,6 @@ class ConnectMysql:
         self.connect.commit()
 
     def create(self):
-        # 若已有数据表weather_year_db，则删除
-        # table_name = 'zx_flow'
-        # self.cursor.execute('DROP TABLE IF EXISTS {}'.format(self.opt.tableName))
-        # 创建数据表，用刚才提取的列索引作为字段
         columns = self.read_csv_columns()
         text = 'CREATE TABLE {}({})'.format(self.opt.tableName, columns)
         print(text)
@@ -152,7 +127,7 @@ class ConnectMysql:
         result = self.cursor.fetchall()
         return len(result) != 0
 
-    def run(self):
+    def start_write_csv(self):
         if ~self.exists():
             self.create()
         self.write_mysql()
