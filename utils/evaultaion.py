6e53b36fe02678909d832d4e16a9ddf5ebd5215f
SailorLee11 <18962364008@163.com>
2022-10-16 16:11:38 +0800

    Update evaultaion.py

--- utils/evaultaion.py
+++ utils/evaultaion.py
@@ -12,6 +12,8 @@ from scipy.optimize import brentq
 from scipy.interpolate import interp1d
 import matplotlib.pyplot as plt
 import os
+import numpy as np
+import pandas as pd
 
 class Eva():
 
@@ -80,3 +82,45 @@ class Eva():
     def auprc(self,labels, scores):
         ap = average_precision_score(labels, scores)
         return ap
+
+    def plot_confusion_matrix(cm, savename, classes, title='Confusion Matrix'):
+        plt.figure(figsize=(12, 8), dpi=500)
+        # plt.figure(figsize=(12, 8), dpi=100)
+        np.set_printoptions(precision=2)
+
+        # 在混淆矩阵中每格的概率值
+        ind_array = np.arange(len(classes))
+        x, y = np.meshgrid(ind_array, ind_array)
+        for x_val, y_val in zip(x.flatten(), y.flatten()):
+            c = cm[y_val][x_val]
+            if c > 0.001:
+                plt.text(x_val, y_val, "%0.2f" % (c,), color='red', fontsize=3, va='center', ha='center')
+
+        plt.imshow(cm, interpolation='nearest', cmap=plt.cm.binary)
+        plt.title(title)
+        plt.colorbar()
+        xlocations = np.array(range(len(classes)))
+        plt.xticks(xlocations, classes, rotation=90)
+        plt.yticks(xlocations, classes)
+        plt.ylabel('Actual label')
+        plt.xlabel('Predict label')
+
+        # offset the tick
+        tick_marks = np.array(range(len(classes))) + 0.5
+        plt.gca().set_xticks(tick_marks, minor=True)
+        plt.gca().set_yticks(tick_marks, minor=True)
+        plt.gca().xaxis.set_ticks_position('none')
+        plt.gca().yaxis.set_ticks_position('none')
+        plt.grid(True, which='minor', linestyle='-')
+        plt.gcf().subplots_adjust(bottom=0.15)
+
+        # show confusion matrix
+        plt.savefig(savename, format='png', dpi=500)
+        plt.show()
+
+    def plot_learning_curves(self,history):
+
+        pd.DataFrame(history.history).plot(figsize = (8,5))
+        plt.grid(True)
+        plt.gca().set_ylim(0,1)
+        plt.show()
