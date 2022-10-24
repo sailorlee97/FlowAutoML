c1ed72d05f0a77cc85f434045376468b0bbb65f2
Zeyi <50068428+sailorlee97@users.noreply.github.com>
2022-10-24 16:00:14 +0800

    updatecsvdb

--- utils/csvdb.py
+++ utils/csvdb.py
@@ -9,33 +9,47 @@
 """
 import pymysql
 import pandas as pd
+import tensorflow as tf
 from sklearn.preprocessing import LabelEncoder
 
-class ConnectMysql:
+class ConnectMysql():
+
     # 金凯威
-    conn = pymysql.connect(
-        user='runtrend',
-        password='4rfv*UHB',
-        host='rm-bp1mv8ua26rj84t32eo.mysql.rds.aliyuncs.com',
-        database='flowfeature',
-        port=3306,
-        charset='utf8mb4',
-    )
-    sql = "select * from test_flowfeature where appname in ('AcFun','aiqiyijisuban','aobidao','aolaxing','Bing','douyin','duoduoshipin','duxiaoshi','elma','fengxingshipin')"
-    df = pd.read_sql(sql, con=conn)
-    print(df)
-    df.to_csv('test.csv', index=False)
-    print('Read from sqlserver table successfully!')
+    def __init__(self):
+        self.conn = pymysql.connect(
+            user='runtrend',
+            password='4rfv*UHB',
+            host='rm-bp1mv8ua26rj84t32eo.mysql.rds.aliyuncs.com',
+            database='flowfeature',
+            port=3306,
+            charset='utf8mb4',
+        )
+        self.issavetocsv = False
 
-    data = pd.read_csv("test.csv", header=0)
+    def get_data(self):
 
-    chuli = data.drop(
-        ["Flow ID", "Src IP", "Src Port", "Dst IP", "Dst Port", "Label", "appversion", "appplatform", "date",
-         "chargeperson"], axis=1, inplace=False)
+        sql = "select * from test_flowfeature where appname in ('AcFun','aiqiyijisuban','aobidao','aolaxing','Bing','douyin','duoduoshipin','duxiaoshi','elma','fengxingshipin')"
 
-    le = LabelEncoder()
-    chuli.iloc[0:, -1] = le.fit_transform(chuli.iloc[0:, -1])
+        df = pd.read_sql(sql, con=self.conn)
+        #print(df)
+        #df.to_csv('test.csv', index=False)
+        print('Read from sqlserver table successfully!')
 
-    print(chuli)
-    chuli.to_csv('testing.csv')
-    print('dl successfully')
+        #data = pd.read_csv("test.csv", header=0)
+
+        process = df.drop(
+            ["Flow ID", "Src IP", "Src Port", "Dst IP", "Dst Port", "Label", "appversion", "appplatform", "date","index",
+             "chargeperson"], axis=1, inplace=False)
+
+        #le = LabelEncoder()
+        #process.iloc[0:, -1] = le.fit_transform(process.iloc[0:, -1])
+
+        print(process)
+        if self.issavetocsv:
+            process.to_csv('testing.csv')
+        print('dl successfully')
+
+
+if __name__ == '__main__':
+    getdata = ConnectMysql()
+    getdata.get_data()
