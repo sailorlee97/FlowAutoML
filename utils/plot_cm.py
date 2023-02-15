c77c051ad74b3dfc72fe0a2dace2ea85a986685f
SailorLee11 <18962364008@163.com>
2023-02-15 16:29:26 +0800

    Create plot_cm.py

---
+++ utils/plot_cm.py
@@ -0,0 +1,53 @@
+import itertools
+
+import numpy as np
+from matplotlib import pyplot as plt
+from sklearn.metrics import confusion_matrix
+
+
+def plot_confusion_matrix(cm,
+                          target_names,
+                          title='Confusion matrix',
+                          cmap=plt.cm.Greens,
+                          normalize=True):
+    accuracy = np.trace(cm) / float(np.sum(cm))
+    misclass = 1 - accuracy
+
+    if cmap is None:
+        cmap = plt.get_cmap('Blues')
+
+    plt.figure(figsize=(15, 12))  # (15,12)
+    plt.imshow(cm, interpolation='nearest', cmap=cmap)
+    plt.title(title)
+    plt.colorbar()
+
+    if target_names is not None:
+        tick_marks = np.arange(len(target_names))
+        plt.xticks(tick_marks, target_names, rotation=45)
+        plt.yticks(tick_marks, target_names)
+
+    if normalize:
+        cm = cm.astype('float') / cm.sum(axis=1)[:, np.newaxis]
+
+    thresh = cm.max() / 1.5 if normalize else cm.max() / 2
+    for i, j in itertools.product(range(cm.shape[0]), range(cm.shape[1])):
+        if normalize:
+            plt.text(j, i, "{:0.4f}".format(cm[i, j]),
+                     horizontalalignment="center",
+                     color="white" if cm[i, j] > thresh else "black")
+        else:
+            plt.text(j, i, "{:,}".format(cm[i, j]),
+                     horizontalalignment="center",
+                     color="white" if cm[i, j] > thresh else "black")
+
+    plt.tight_layout()
+    plt.ylabel('True label')
+    plt.xlabel('Predicted label\naccuracy={:0.4f}; misclass={:0.4f}'.format(accuracy, misclass))
+    plt.savefig('./confusionmatrix.png',dpi=500)  # dpi分辨率
+    plt.show()
+
+
+def plot_conf(y_pre, y_val, labels):
+    conf_mat = confusion_matrix(y_true=y_val, y_pred=y_pre)
+    print(conf_mat)
+    plot_confusion_matrix(conf_mat, normalize=False, target_names=labels, title='Confusion Matrix')
