ee18751fed4408ac857101eb591c3ade42d4586f
SailorLee11 <18962364008@163.com>
2023-03-22 21:24:11 +0800

    Update plot_cm.py

--- utils/plot_cm.py
+++ utils/plot_cm.py
@@ -1,9 +1,11 @@
 import itertools
+import random
 import matplotlib as mpl
 import numpy as np
 from matplotlib import pyplot as plt
-from sklearn.metrics import confusion_matrix
-
+from numpy import interp
+from sklearn.metrics import confusion_matrix, roc_curve, auc
+import matplotlib.colors as mcolors
 
 def plot_confusion_matrix(cm,
                           target_names,
@@ -52,3 +54,73 @@ def plot_conf(y_pre, y_val, labels):
     conf_mat = confusion_matrix(y_true=y_val, y_pred=y_pre)
     print(conf_mat)
     plot_confusion_matrix(conf_mat, normalize=False, target_names=labels, title='Confusion Matrix')
+
+def multi_roc(y_label, y_score,labels):
+    """
+      将多分类的roc图输出到一张图上
+
+      Args:
+          y_label: 多个分类的真实标签
+          y_score: 多个分类的预测标签
+          labels: 多个分类的名称
+
+      """
+    # 计算每一类的ROC
+    n_classes=len(y_label[0])
+    fpr = dict()
+    tpr = dict()
+    roc_auc = dict()
+    for i in range(n_classes):
+        fpr[i], tpr[i], _ = roc_curve(y_label[:, i], y_score[:, i])
+        roc_auc[i] = auc(fpr[i], tpr[i])
+
+    # micro
+    print(y_label.ravel())
+    fpr["micro"], tpr["micro"], _ = roc_curve(y_label.ravel(), y_score.ravel())
+    roc_auc["micro"] = auc(fpr["micro"], tpr["micro"])
+
+    # macro
+    # First aggregate all false positive rates
+    all_fpr = np.unique(np.concatenate([fpr[i] for i in range(n_classes)]))
+    # Then interpolate all ROC curves at this points
+    mean_tpr = np.zeros_like(all_fpr)
+    for i in range(n_classes):
+        mean_tpr += interp(all_fpr, fpr[i], tpr[i])
+    # Finally average it and compute AUC
+    mean_tpr /= n_classes
+    fpr["macro"] = all_fpr
+    tpr["macro"] = mean_tpr
+    roc_auc["macro"] = auc(fpr["macro"], tpr["macro"])
+
+    # Plot all ROC curves
+    lw = 2
+    plt.figure(dpi=500)
+    plt.rcParams['font.sans-serif'] = ['SimHei']  # 指定默认字体
+    plt.rcParams['axes.unicode_minus'] = False  # 解决保存图像是负号'-'显示为方块的问题
+    plt.plot(fpr["micro"], tpr["micro"],
+             label='micro-average ROC curve (area = {0:0.2f})'
+                   ''.format(roc_auc["micro"]),
+             color='deeppink', linestyle=':', linewidth=4)
+
+    plt.plot(fpr["macro"], tpr["macro"],
+             label='macro-average ROC curve (area = {0:0.2f})'
+                   ''.format(roc_auc["macro"]),
+             color='navy', linestyle=':', linewidth=4)
+
+    colors = list(mcolors.CSS4_COLORS.keys())  # 颜色变化
+
+    for i in range(n_classes):
+        colors_i = random.randint(1,148)
+        plt.plot(fpr[i], tpr[i], color=mcolors.CSS4_COLORS[colors[colors_i]], lw=lw,
+                 label='ROC curve of class {0} (area = {1:0.2f})'
+                       ''.format(labels[i], roc_auc[i]))
+
+    plt.plot([0, 1], [0, 1], '--', lw=1, color='grey')
+    plt.xlim([0.0, 1.0])
+    plt.ylim([0.0, 1.05])
+    plt.xlabel('False Positive Rate',fontsize=20)
+    plt.ylabel('True Positive Rate',fontsize=20)
+    plt.title('multi-calss ROC', fontsize=25)
+    plt.legend(loc="lower right")
+    # plt.show()
+    plt.savefig('multi_class_roc.png')
