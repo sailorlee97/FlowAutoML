657f35d5edd527061c58eb0708d3ebd09a1745f1
SailorLee11 <18962364008@163.com>
2023-03-24 15:17:11 +0800

    Create five_label_c.h

---
+++ outputs/data_c/five_label_c.h
@@ -0,0 +1,14 @@
+#ifndef _EXAMPLE_FILE_H
+#define _EXAMPLE_FILE_H
+
+#include "embedia.h"
+
+
+
+        uint16_t sample_data_id[] = { 
+          0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 
+  0, 0, 0, 0, 0, 0, 0, 0, 0, 0
+};
+
+
+#endif
