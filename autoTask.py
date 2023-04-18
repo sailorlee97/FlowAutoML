ed4ec899919c0303c2934290116432fba3ed8fa6
SailorLee11 <18962364008@163.com>
2023-04-18 19:51:47 +0800

    Update autoTask.py

--- autoTask.py
+++ autoTask.py
@@ -7,8 +7,9 @@
 @FileName: autoTask.py
 @Software: PyCharm
 """
-from keras.layers import Normalization
+
 from keras.saving.save import load_model
+from pandas import DataFrame
 from sklearn.metrics import classification_report, accuracy_score
 from sklearn.preprocessing import LabelEncoder
 from sklearn.model_selection import train_test_split
@@ -24,10 +25,9 @@ from sklearn.metrics import f1_score
 from utils.selectfeatures import selectfeature
 import pandas as pd
 import os
+import math
 import numpy as np
 import time
-import csv
-import math
 import tensorflow as tf
 
 
@@ -123,7 +123,6 @@ class autotask():
 
         return scaler.transform(x)
 
-
     def test_single(self, model_path, csv_path, name, label_number, isall=False):
         """
           测试单类预测准确率
@@ -178,7 +177,7 @@ class autotask():
         dataArray = dataframe.values
         X = np.expand_dims(dataArray.astype(float), axis=2)
         lenx = len(X)
-        newx = X.reshape((lenx, 7, 7, 1))
+        newx = X.reshape((lenx, 5, 5, 1))
         true_label = np.ones(lenx) * label_number
         # 预测
         predict = test_model.predict(newx)
@@ -275,21 +274,28 @@ class autotask():
         # 输出文件大小（以MB为单位）
         file_size = os.path.getsize(model_path)
         print(f"Model size: {file_size / (1024 * 1024)} MB")
-        # 获取特征
-        alist = []
-        if os.path.exists('./log/features'):
 
-            for line in open("./log/features"):
-                line = line.strip('\n')
-                alist.append(line)
-        else:
-            raise Exception('dont exist folder!')
-        print(alist)
         # 提取数据
         # 从数据库中拿数据
         dataframe_test = self.mysqldata.total_get_data(app=self.opt.appsql,
                                                    featurebase=tableName)
-        df = dataframe_test[alist]
+        # 获取特征
+        if self.opt.enable_all_features == 'no':
+            alist = []
+            if os.path.exists('./log/features'):
+
+                for line in open("./log/features"):
+                    line = line.strip('\n')
+                    alist.append(line)
+            else:
+                raise Exception('dont exist folder!')
+            print(alist)
+
+            df = dataframe_test[alist]
+        else:
+            df = dataframe_test.drop(columns=['s_fHeaderBytes','s_bHeaderBytes','s_lenSd','s_fLenSd','s_fIATSd',
+                                                 's_bLenSd','s_bIATSd','s_flowIATSd','s_idleSd','s_activeSd'])
+            alist = df.columns
         # 数据处理
         dataframe = df.replace([np.inf, -np.inf], np.nan).dropna().copy()
 
@@ -361,7 +367,7 @@ class autotask():
         X1 = np.expand_dims(dataArray.astype(float), axis=2)
 
         lenx = len(X1)
-        newx = X1.reshape((lenx, 7, 7, 1))
+        newx = X1.reshape((lenx, int(math.sqrt(self.opt.number_features)), int(math.sqrt(self.opt.number_features)), 1))
         print(newx.shape)
         y_train = keras.utils.to_categorical(newlabels, self.opt.nclass)
 
@@ -377,7 +383,7 @@ class autotask():
         for _ in featurelist[:-1]:
             dataframe[_] = dataframe[_].map(lambda x: (x - data_mean[_]) / data_std[_])
 
-        return dataframe
+        return dataframe[featurelist]
 
     def _select_features(self,path):
         '''
@@ -395,17 +401,23 @@ class autotask():
         dataframe_pd = df.copy()
         dataframe0 = dataframe_pd.drop(dataframe_pd.columns[0], axis=1)
         dataframe = dataframe0.replace([np.inf, -np.inf], np.nan).dropna()
-        # dataframe_del = dataframe.drop(['port1','port2','startSec','startNSec','endSec','endNSec'],axis=1)
+        dataframe = dataframe.drop(['s_fMinSegSize','s_lenMin','s_fPayloadSum','s_bytesPerSecond','s_fLenMin','s_bwdSegSizeAvg','payloadSum','s_ack','s_bHeaderBytes','s_bPktsCounts','s_bLenMean',
+        's_bPayloadSum','s_flowIATMean','s_fActDataPkts','s_PktsPerSecond','s_fPktsCounts','s_fPktsPerSecond','s_bLenSd','s_bPktsPerSecond','s_bIATMean','proto','s_psh','s_bIATTotal','s_fBulkDuration','s_fPSHCnt']
+        ,axis=1)
         labels = dataframe.pop('appname')
         # propress labels
         le = LabelEncoder()
         newlabels = le.fit_transform(labels)
 
+        columns = dataframe.columns
+        # columns = columns[:-1]
+        df_ = self._process_standard(dataframe)
+        dataframe = DataFrame(data=df_, columns=columns)
         sf = selectfeature()
-        secorndfeatrues = sf.search_corrlate_features(dataframe, newlabels)
+        secorndfeatrues = sf.search_corrlate_features(dataframe, newlabels,self.opt.number_features)
         newdataframe = dataframe[secorndfeatrues]
         feauturesimportance = sf.treemodel(newdataframe, newlabels)
-        features = list(feauturesimportance[:49]['Features'])
+        features = list(feauturesimportance[:self.opt.number_features]['Features'])
         features.append('appname')
 
         return features
@@ -461,41 +473,58 @@ class autotask():
         # le = LabelEncoder()
         # newlabels = le.fit_transform(labels)
         # newdata = pd.get_dummies(dataframe)
-        featurelist = []
-        time_sf = 0
-        if os.path.exists('./log/features'):
+        if self.opt.enable_all_features == 'no':
+            featurelist = []
+            time_sf = 0
+            if os.path.exists('./log/features'):
 
-            for line in open("./log/features"):
-                line = line.strip('\n')
-                featurelist.append(line)
+                for line in open("./log/features"):
+                    line = line.strip('\n')
+                    featurelist.append(line)
+            else:
+
+                time_feature_s = time.time()
+
+                featurelist = self._select_features('./csv_data/dataframe%d.csv'%(self.opt.nclass))
+
+                time_feature_e = time.time()
+                time_sf += time_feature_e-time_feature_s
+                print('time of select features: {} min'.format(time_sf/60))
+
+                f = open('./log/features', 'a')
+                for i in featurelist:
+                    f.write(i)
+                    f.write('\n')
+                f.close()
+
+            dataframe1 = dataframe[featurelist]
         else:
-
-            time_feature_s = time.time()
-
-            featurelist = self._select_features('./csv_data/dataframe%d.csv'%(self.opt.nclass))
-
-            time_feature_e = time.time()
-            time_sf += time_feature_e-time_feature_s
-            print('time of select features: {} min'.format(time_sf/60))
-
-            f = open('./log/features', 'a')
-            for i in featurelist:
-                f.write(i)
-                f.write('\n')
-            f.close()
-
-        dataframe1 = dataframe[featurelist]
-
+            dataframe1 = dataframe.drop(columns=['s_fHeaderBytes','s_bHeaderBytes','s_lenSd','s_fLenSd','s_fIATSd',
+                                                 's_bLenSd','s_bIATSd','s_flowIATSd','s_idleSd','s_activeSd'
+                                                 ])
+            featurelist = dataframe1.columns
+            time_sf = 0
         # 特征增强
-        data_std = dataframe1.std()
-        pd_data_std = pd.DataFrame(data_std)
-        pd_data_std.to_csv('./log/std.csv')
-        data_mean = dataframe1.mean()
-        pd_data_mean = pd.DataFrame(data_mean)
-        pd_data_mean.to_csv('./log/mean.csv')
+        if os.path.exists('./log/std.csv') or os.path.exists('./log/mean.csv'):
 
-        for _ in featurelist[:-1]:
-            dataframe1[_] = dataframe1[_].map(lambda x : (x-data_mean[_])/data_std[_])
+            pd_std = pd.read_csv('./log/std.csv')
+            pd_std.columns = ['key', 'value']
+            dict_std = dict(zip(pd_std['key'], pd_std['value']))
+            pd_mean = pd.read_csv('./log/mean.csv')
+            pd_mean.columns = ['key', 'value']
+            dict_mean = dict(zip(pd_mean['key'], pd_mean['value']))
+            dataframe1 = self._process_stand(featurelist, dataframe, dict_mean, dict_std)
+
+        else:
+            data_std = dataframe1.std()
+            pd_data_std = pd.DataFrame(data_std)
+            pd_data_std.to_csv('./log/std.csv')
+            data_mean = dataframe1.mean()
+            pd_data_mean = pd.DataFrame(data_mean)
+            pd_data_mean.to_csv('./log/mean.csv')
+
+            for _ in featurelist[:-1]:
+                dataframe1[_] = dataframe1[_].map(lambda x : (x-data_mean[_])/data_std[_])
         # print(a)
 
         print(dataframe1.shape)
@@ -513,7 +542,7 @@ class autotask():
         reskey = list(res_te.keys())
         # print(reskey)
 
-        cnnmodel = autonetworks(self.opt.nclass, 49)
+        cnnmodel = autonetworks(self.opt.nclass, self.opt.number_features)
         model = cnnmodel.buildmodels()
 
         time_train_s = time.time()
@@ -550,26 +579,6 @@ class autotask():
 
         return f1
 
-    # alas
-    # def train_predit(self):
-    #     # 重新抓取新的数据集
-    #     X_train, X_test, y_train, y_test = self.obtaindata()
-    #     # train_ds, val_ds, test_ds = self.obtaindata_new()
-    #     inp_size = X_train.shape[1]
-    #
-    #     time_s = time.time()
-    #     cnnmodel = autonetworks(self.opt.nclass, inp_size)
-    #     model = cnnmodel.buildmodels()
-    #     model.fit(X_train, y_train, epochs=self.opt.epochs, batch_size=64)
-    #     # estimator = KerasClassifier(build_fn=cnnmodel.buildmodels, epochs=self.opt.epochs, batch_size=64, verbose=1)
-    #     # estimator.fit(X_train, y_train)
-    #     time_e = time.time()
-    #     train_time = time_e - time_s
-    #     print("train time:", train_time)
-    #     y_pred = model.predict(y_test)
-    #     print(classification_report(y_test.argmax(-1), y_pred.argmax(-1)))
-
-
     # 此处设计在线学习的module
     def process_run(self):
         if (self.opt.isInitialization == 'yes'):
@@ -577,9 +586,9 @@ class autotask():
             if not os.path.exists(self.opt.model_file):
                 auc = self._obtain_data_train_test()
             else:
-                # self.test_all('./savedmodels/model_0403_5_49.h5', './csv_data/dataframe%d.csv'%(self.opt.nclass))
-                self.test_new_all('./savedmodels/model_0407_5_49.h5', '5APP_flowfeature_update_int')
-                # self.test_all(self.opt.model_file, './csv_data/dataframe%d.csv' % (self.opt.nclass))
+                # self.test_all('./savedmodels/model_0407_5_49.h5', './csv_data/dataframe%d.csv'%(self.opt.nclass))
+                # self.test_new_all('./savedmodels/model_0413_5_25.h5', '5APP_flowfeature_update_int')
+                self.test_all(self.opt.model_file, './csv_data/dataframe%d.csv' % (self.opt.nclass))
                 # self.test_single(self.opt.model_file, './csv_data/yuanshen_v3.5.0_Android_20230329_jkw.csv', '原神', 0, isall=False)
 
         elif (self.opt.isInitialization == 'no'):
