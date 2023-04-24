801d083b571364b4ba58a0283b03d41c48887d92
SailorLee11 <18962364008@163.com>
2023-04-24 15:37:21 +0800

    Update options.py

--- options.py
+++ options.py
@@ -20,28 +20,28 @@ class Options():
         self.parser.add_argument('--isInitialization', default='no',
                                  help='yes - model train;no - we will load model trained.')
         self.parser.add_argument('--label', default='appname', help='data class labels.')
-        self.parser.add_argument('--epochs', default=100, help='number of epochs')
-        self.parser.add_argument('--nclass', default=17, help='number class labels.')
-        self.parser.add_argument('--batch_size', default=36, help='batch_size.')
+        self.parser.add_argument('--epochs', default=40, help='number of epochs')
+        self.parser.add_argument('--nclass', default=11, help='number class labels.')
+        self.parser.add_argument('--batch_size', default=64, help='batch_size.')
+        self.parser.add_argument('--embedd_data_type', default='FLOAT', help='FLOAT, FIXED32, FIXED16, and FIXED8.')
+        self.parser.add_argument('--enable_all_features', default='no',
+                                 help='If all features are enabled, the feature selection method is not required.')
 
         # Model curing parameter
         self.parser.add_argument('--output_folder', default='outputs/', help='Path of output.')
-        self.parser.add_argument('--enable_all_features', default='no', help='If all features are enabled, the feature selection method is not required.')
-        self.parser.add_argument('--number_features', default=36, help='number_features name.')
-        self.parser.add_argument('--project_name', default='model_0417_17_36', help='project name.')
-        self.parser.add_argument('--model_file', default='savedmodels/model_0417_17_36.h5', help='Path of model.')
+        self.parser.add_argument('--number_features', default=49, help='number_features name.')
+        self.parser.add_argument('--project_name', default='model_0422_11_49', help='project name.')
+        self.parser.add_argument('--model_file', default='savedmodels/model_0422_11_49.h5', help='Path of model.')
         self.parser.add_argument('--apps',
-                                 default=['王者荣耀','和平精英','哈利波特魔法觉醒','原神','央视影音','战双帕弥什',
-                                        'QQ飞车','狂野飙车9竞速传奇','腾讯会议','网易会议','猿辅导','抖音','bilibili','爱奇艺',
-                                            'VR', 'QQ音乐', '淘宝'],
+                                 default=['王者荣耀','和平精英','战双帕弥什','原神','网易会议',
+                                          '猿辅导','抖音','bilibili','爱奇艺','picoshipin', 'QQ音乐'],
                                  help='Select the application of the classification. The order apps here is the order of the labels.')
 
         # database
         self.parser.add_argument('--port', default=26618, help='connect server port.')
         self.parser.add_argument('--appsql',
-                                 default=('王者荣耀','和平精英','哈利波特魔法觉醒','原神','央视影音','战双帕弥什',
-                                        'QQ飞车','狂野飙车9竞速传奇','腾讯会议','网易会议','猿辅导','抖音','bilibili','爱奇艺',
-                                            'VR', 'QQ音乐', '淘宝'),
+                                 default=('王者荣耀','和平精英','战双帕弥什','原神','网易会议',
+                                          '猿辅导','抖音','bilibili','爱奇艺','picoshipin', 'QQ音乐'),
                                  help='Applications selected from the database.')
         self.parser.add_argument('--host', default='sh-cynosdbmysql-grp-5dmxbh9a.sql.tencentcdb.com',
                                  help='server ip addr.')
