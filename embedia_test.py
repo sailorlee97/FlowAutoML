9e1082ce633926ffd95a85fe0dbfbc8ab57f73ec
SailorLee11 <18962364008@163.com>
2023-04-24 15:28:35 +0800

    Update embedia_test.py

--- embedia_test.py
+++ embedia_test.py
@@ -61,7 +61,15 @@ class embediaModel(BaseClassification):
 
         options = ProjectOptions()
         options.project_type = ProjectType.C
-        options.data_type = ModelDataType.FLOAT
+
+        transformdict = {
+            'FLOAT': ModelDataType.FLOAT,
+            'FIXED32': ModelDataType.FIXED32,
+            'FIXED16': ModelDataType.FIXED16,
+            'FIXED8': ModelDataType.FIXED8,
+        }
+        
+        options.data_type = transformdict.get(self.opt.embedd_data_type)
         options.debug_mode = DebugMode.DISCARD
         options.example_data = sample
         options.example_comment = comment
@@ -82,47 +90,6 @@ class embediaModel(BaseClassification):
 #
 # df0 = pd.read_csv('.\csv_data\dataframe10.csv')
 
-# 64个特性
-# df=df0[[  'Bwd_Pkt_Len_Std', 'Flow_Byts/s', 'Flow_Pkts/s', 'Flow_IAT_Mean',
-#        'Flow_IAT_Std', 'Flow_IAT_Max', 'Flow_IAT_Min', 'Fwd_IAT_Tot',
-#        'Fwd_IAT_Mean', 'Fwd_IAT_Std', 'Fwd_IAT_Max', 'Fwd_IAT_Min',
-#        'Bwd_IAT_Tot', 'Bwd_IAT_Mean', 'Bwd_IAT_Std', 'Bwd_IAT_Max',
-#        'Bwd_IAT_Min', 'Fwd_PSH_Flags', 'Bwd_PSH_Flags', 'Fwd_URG_Flags',
-#        'Bwd_URG_Flags', 'Fwd_Header_Len', 'Bwd_Header_Len', 'Fwd_Pkts/s',
-#        'Bwd_Pkts/s', 'Pkt_Len_Min', 'Pkt_Len_Max', 'Pkt_Len_Mean',
-#        'Pkt_Len_Std', 'Pkt_Len_Var', 'FIN_Flag_Cnt', 'SYN_Flag_Cnt',
-#        'RST_Flag_Cnt', 'PSH_Flag_Cnt', 'ACK_Flag_Cnt', 'URG_Flag_Cnt',
-#        'CWE_Flag_Count', 'ECE_Flag_Cnt', 'Down/Up_Ratio', 'Pkt_Size_Avg',
-#        'Fwd_Seg_Size_Avg', 'Bwd_Seg_Size_Avg', 'Fwd_Byts/b_Avg',
-#        'Fwd_Pkts/b_Avg', 'Fwd_Blk_Rate_Avg', 'Bwd_Byts/b_Avg',
-#        'Bwd_Pkts/b_Avg', 'Bwd_Blk_Rate_Avg', 'Subflow_Fwd_Pkts',
-#        'Subflow_Fwd_Byts', 'Subflow_Bwd_Pkts', 'Subflow_Bwd_Byts',
-#        'Init_Fwd_Win_Byts', 'Init_Bwd_Win_Byts', 'Fwd_Act_Data_Pkts',
-#        'Fwd_Seg_Size_Min', 'Active_Mean', 'Active_Std', 'Active_Max',
-#        'Active_Min', 'Idle_Mean', 'Idle_Std', 'Idle_Max', 'Idle_Min',
-#        'appname']]
-
-# 25个特征
-# df = df0[
-#        ['ACK_Flag_Cnt', 'Active_Max', 'Active_Mean', 'Active_Min', 'Active_Std', 'Bwd_Blk_Rate_Avg',
-#         'Bwd_Byts/b_Avg', 'Bwd_Header_Len', 'Bwd_IAT_Max', 'Bwd_IAT_Mean', 'Bwd_IAT_Min', 'Bwd_IAT_Std',
-#         'Bwd_IAT_Tot', 'Bwd_PSH_Flags', 'Bwd_Pkt_Len_Max', 'Bwd_Pkt_Len_Mean', 'Bwd_Pkt_Len_Min',
-#         'Bwd_Pkt_Len_Std', 'Bwd_Pkts/b_Avg', 'Bwd_Pkts/s', 'Bwd_Seg_Size_Avg', 'Bwd_URG_Flags', 'CWE_Flag_Count',
-#         'Down/Up_Ratio', 'ECE_Flag_Cnt', 'appname']]
-
-# 36个特征
-# df = df0[[
-#     'Pkt_Len_Std', 'Pkt_Len_Var', 'FIN_Flag_Cnt', 'SYN_Flag_Cnt',
-#     'RST_Flag_Cnt', 'PSH_Flag_Cnt', 'ACK_Flag_Cnt', 'URG_Flag_Cnt',
-#     'CWE_Flag_Count', 'ECE_Flag_Cnt', 'Down/Up_Ratio', 'Pkt_Size_Avg',
-#     'Fwd_Seg_Size_Avg', 'Bwd_Seg_Size_Avg', 'Fwd_Byts/b_Avg',
-#     'Fwd_Pkts/b_Avg', 'Fwd_Blk_Rate_Avg', 'Bwd_Byts/b_Avg',
-#     'Bwd_Pkts/b_Avg', 'Bwd_Blk_Rate_Avg', 'Subflow_Fwd_Pkts',
-#     'Subflow_Fwd_Byts', 'Subflow_Bwd_Pkts', 'Subflow_Bwd_Byts',
-#     'Init_Fwd_Win_Byts', 'Init_Bwd_Win_Byts', 'Fwd_Act_Data_Pkts',
-#     'Fwd_Seg_Size_Min', 'Active_Mean', 'Active_Std', 'Active_Max',
-#     'Active_Min', 'Idle_Mean', 'Idle_Std', 'Idle_Max', 'Idle_Min',
-#     'appname']]
 
 # dataframe = df.copy()
 # labels = dataframe.pop('appname')
