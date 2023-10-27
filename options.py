46231659651f926c5ac40beefc23fcb390024afb
Zeyi <50068428+sailorlee97@users.noreply.github.com>
2023-10-27 09:51:17 +0800

    Update options.py

--- options.py
+++ options.py
@@ -20,9 +20,10 @@ class Options():
         self.parser.add_argument('--isInitialization', default='yes',
                                  help='yes - model train;no - we will load model trained.')
         self.parser.add_argument('--label', default='appname', help='data class labels.')
-        self.parser.add_argument('--epochs', default=200, help='number of epochs')
-        self.parser.add_argument('--nclass', default=15, help='number class labels and background.')
-        self.parser.add_argument('--batch_size', default=256, help='batch_size.')
+        self.parser.add_argument('--epochs', default=100, help='number of epochs')
+        self.parser.add_argument('--nclass', default=8, help='number class labels and background.')
+        self.parser.add_argument('--batch_size', default=16, help='batch_size.')
+        self.parser.add_argument('--lr', default=0.001, help='learning rate.',type=int)
         self.parser.add_argument('--embedd_data_type', default='FLOAT', help='FLOAT, FIXED32, FIXED16, and FIXED8.')
         self.parser.add_argument('--enable_all_features', default='no',
                                  help='If all features are enabled, the feature selection method is not required.')
@@ -30,25 +31,22 @@ class Options():
         # Model curing parameter
         self.parser.add_argument('--output_folder', default='outputs/', help='Path of output.')
         self.parser.add_argument('--number_features', default=49, help='number_features name.')
-        self.parser.add_argument('--project_name', default='model_10_10_49', help='project name.')
-        self.parser.add_argument('--model_file', default='savedmodels/model_1010_15_49.h5', help='Path of model.')
+        self.parser.add_argument('--project_name', default='model_1027_8_49', help='project name.')
+        self.parser.add_argument('--model_file', default='savedmodels/model_1027_8_49.h5', help='Path of model.')
         self.parser.add_argument('--apps',
-                                 default=['QQ音乐', '百度贴吧','腾讯会议','虎牙直播','王者荣耀','腾讯视频','哔哩哔哩',
-                                          '抖音','中国大学MOOC','金铲铲之战','爱奇艺','香肠派对','和平精英','优酷视频'],
+                                 default=['原神','王者荣耀','和平精英','香肠派对','崩坏：星穹铁','英雄联盟手游','蛋仔派对'],
                                  help='Select the application of the classification. The order apps here is the order of the labels.')
-
         # database
         self.parser.add_argument('--port', default=26618, help='connect server port.')
         self.parser.add_argument('--appsql',
-                                 default=('QQ音乐', '百度贴吧','腾讯会议','虎牙直播','王者荣耀','腾讯视频','哔哩哔哩',
-                                          '抖音','中国大学MOOC','金铲铲之战','爱奇艺','香肠派对','和平精英','优酷视频'),
+                                 default=('原神','王者荣耀','和平精英','香肠派对','崩坏：星穹铁','英雄联盟手游','蛋仔派对'),
                                  help='Applications selected from the database.')
         self.parser.add_argument('--host', default='sh-cynosdbmysql-grp-5dmxbh9a.sql.tencentcdb.com',
                                  help='server ip addr.')
         self.parser.add_argument('--username', default='runtrend', help='the name that server log in.')
         self.parser.add_argument('--password', default='*****', help='the password that server log in.')
         self.parser.add_argument('--databaseName', default='flowfeature', help='the name that database log in.')
-        self.parser.add_argument('--tableName', default='ZTE_AP_flowfeature_accuratelabel_sniffer', help='the name of table.')
+        self.parser.add_argument('--tableName', default='ZTE_AP_flowfeature_accuratelabel_sniffer_repaired', help='the name of table.')
         self.parser.add_argument('--charset', default='utf8mb4', help='the name of word.')
         self.opt = None
 
